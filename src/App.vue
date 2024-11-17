93c982e4d61d9e0811321421cad41efd7c9a3390
Shehbaz Majeed <sshehbazmajeed@gmail.com>
2024-11-17 13:32:35 +0500

    Updated bacis version of codebase.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <router-view />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import Header from '@/components/Header.vue'
+  import Footer from '@/components/Footer.vue'
+  export default {
+    components: { Header, Footer },
+  }
+</script>
---
+++ src/pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-card>
+    <h1>About Template</h1>
+  </v-card>
+</template>
